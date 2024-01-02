443c629723a236cc43af70ccbac34a55bb8dc5cd
Patrick Hermann <patrick.hermann@sva.de>
2024-01-02 11:02:25 +0000

    tuned go operator docs

--- variables.tf
+++ variables.tf
@@ -1,23 +1,28 @@
 variable "minio_region" {
   description = "Default MINIO region"
   default     = "eu-central-1"
+  type        = string
 }
 
 variable "minio_ssl" {
   description = "MINIO SSL"
   default     = false
+  type        = bool
 }
 
 variable "minio_server" {
   description = "Default MINIO host and port"
+  type        = string
 }
 
 variable "minio_user" {
   description = "MINIO user"
+  type        = string
 }
 
 variable "minio_password" {
   description = "MINIO password"
+  type        = string
 }
 
 variable "buckets" {
